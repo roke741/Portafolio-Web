3c626859ca4d00bbb204a21d861ffa402dd7a0cf
roke741 <jhordieroque741@gmail.com>
2023-06-04 01:11:12 -0500

    Primer version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Inject, InjectionToken } from '@angular/core';
+export const LOCAL_STORAGE = new InjectionToken<Storage>('localStorage');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [{ provide: LOCAL_STORAGE, useValue: localStorage }]
+
 })
 export class AppComponent {
   title = 'portafolio-web';
+  darkModeEnabled?:boolean;
+  constructor(@Inject(LOCAL_STORAGE) private localStorage: Storage) {
+    this.darkModeEnabled = JSON.parse(this.localStorage.getItem('darkModeEnabled') ?? 'false') ;
+    if(this.darkModeEnabled == true){
+      document.body.classList.toggle('dark-mode')
+    }
+  }
+
+  toggleDarkTheme(): void {
+    document.body.classList.toggle('dark-mode')
+    this.darkModeEnabled = !this.darkModeEnabled;
+    this.localStorage.setItem('darkModeEnabled', JSON.stringify(this.darkModeEnabled));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import {NgxTypedJsModule} from 'ngx-typed-js';
+
+import {NgsRevealModule} from 'ngx-scrollreveal';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgxTypedJsModule,
+    NgsRevealModule
   ],
   providers: [],
   bootstrap: [AppComponent]
